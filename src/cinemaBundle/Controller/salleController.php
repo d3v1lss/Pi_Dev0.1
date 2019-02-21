238bf4104486f1b2e327bd44e7130343eae768e9
HoussemBouzid <houssem.bouzid@esprit.tn>
2019-02-21 03:26:41 +0100

    update

--- src/cinemaBundle/Controller/salleController.php
+++ src/cinemaBundle/Controller/salleController.php
@@ -17,4 +17,48 @@ class salleController extends Controller
         return $this->render('@cinema/Default/readsalle.html.twig', array('salle' => $salle));
 
     }
+
+    public function createAction(Request $request)
+
+    {
+
+        $film = new film();
+
+        $form = $this->createForm(filmType::class, $film);
+
+        $form = $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ($form->isValid()) {
+            $em->persist($film);
+            $em->flush();
+
+
+            return $this->redirectToRoute('readsalle');
+
+        }
+        return $this->render('@cinema/Default/ajout.html.twig', array('formulaire' => $form->createView()));
+
+    }
+
+
+    public function updateAction($id, Request $request)
+    {
+        $salle = $this->getDoctrine()->getRepository(salle::class)->find($id);
+
+        $form = $this->createForm(salleType::class, $salle);
+
+        $form = $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ($form->isValid()) {
+
+            $em->flush();
+            return $this->redirectToRoute('readsalle');
+        }
+        return $this->render('@cinema/Default/ajout.html.twig', array('formulaire' => $form->createView()));
+    }
+
 }
