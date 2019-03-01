bf8caaa3a4e840967158031b1cd36319a2a2c612
Dorra mahouachi <dorra.mahouachi@esprit.tn>
2019-03-01 01:43:22 +0100

    dorra

--- src/cinemaBundle/Tests/Controller/favorisControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace cinemaBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class favorisControllerTest extends WebTestCase
-{
-}
--- src/cinemaBundle/cinemaBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace cinemaBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class cinemaBundle extends Bundle
-{
-}
