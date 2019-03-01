c2f2c808980d2e5b2b9748dba1a3d0a46f3e5323
HoussemBouzid <houssem.bouzid@esprit.tn>
2019-03-01 04:09:47 +0100

    rating

---
+++ src/cinemaBundle/Form/rateType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace cinemaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class rateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('note')
+        ->add('save',SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'cinemaBundle\Entity\rate'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'cinemabundle_rate';
+    }
+
+
+}
